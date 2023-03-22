7afda4e6ee9edf500966e718a84aa09bf90d3625
MeetKo <55168454+MeetKo@users.noreply.github.com>
2023-03-22 16:50:07 -0400

    working on intro sequence

---
+++ src/Intro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sunrise from 'components./Sunrise';
+
+const Intro = () => {
+  return (
+    <div>
+      <Sunrise />
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Intro;
